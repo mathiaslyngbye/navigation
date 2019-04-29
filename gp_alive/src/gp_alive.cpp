1fcffafb31519e6ff5fb978acaa65d98a83b6251
martinjakobsgaard <martin.jakobsgaard@gmail.com>
2019-04-29 15:16:12 +0200

    its broken

---
+++ gp_alive/src/gp_alive.cpp
@@ -0,0 +1,21 @@
+#include "ros/ros.h"
+#include "../include/gp_alive/AddTwoInts.h"
+
+bool add(beginner_tutorials::AddTwoInts::Request  &req,
+         beginner_tutorials::AddTwoInts::Response &res)
+{
+  res.sum = req.a + req.b;
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "add_two_ints_server");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+  ROS_INFO("Ready to add two ints.");
+  ros::spin();
+
+  return 0;
+}
