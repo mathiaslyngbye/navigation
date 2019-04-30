bdd09615a16fad7885e53e13b0c85c240c397444
mathiaslyngbye <malyn16@student.sdu.dk>
2019-04-30 18:44:10 +0200

    Modified gp_manager to check for alive node

--- gp_manager/src/manager.cpp
+++ gp_manager/src/manager.cpp
@@ -18,22 +18,55 @@ int main(int argc, char **argv)
     bool exists = false;
     std::string command = "";
 
+    // Create strong vector for holding "rosnode list"
+    ros::V_string v;
+
     while(ros::ok())
     {
-        // Args; service_name and print_failure_reason.
-        exists = ros::service::exists("/global_planner/make_plan", true);	    
+        // Reset exists flag
+        exists = false;        
 
+        // Clear previous list of nodes
+        v.clear();   
+        
+        // get ros nodes on current master
+        if(!ros::master::getNodes(v))
+        {
+            ROS_INFO("ROS master failed to get node list...");
+        }
+        else
+        {
+            for(int i = 0; i < v.size(); i++)
+            {
+                if(v[i] == "/alive")
+                {
+                    exists = true;
+                    break;
+                }    
+            }
+        }
+        
         if(!exists)
         {
-            ROS_INFO("global_planner missing on ROS network. Spawning locally.");
-            command = "kubectl apply -f " + pod_location + pod_name;
-            system(command.c_str());
-            ros::Duration(5).sleep();
-            //system("roslaunch gp_launch gp_navfn_node.launch");
+            ROS_INFO("Remote global_planner missing on ROS network.");
+            if(!ros::service::exists("/global_planner/make_plan")
+            {
+                ROS_INFO("Local global_planner missing on ROS network.");
+                ROS_INFO("Spawning local global_panner locally.");
+                //command = "kubectl apply -f " + pod_location + pod_name;
+                //system(command.c_str());
+                ros::Duration(10).sleep();
+            }
+            else
+            {
+                ROS_INFO("Local global_planner exists on ROS network.");
+                ROS_INFO("Doing nothing...");
+            }
         }
         else 
         {
-            ROS_INFO("global_planner exists on ROS network. Doing nothing...");
+            ROS_INFO("Remote global_planner exists on ROS network.");
+            ROS_INFO("Removing local global_planners...");
             //command = "kubectl delete -f " + pod_location + pod_name;
             //system(command.c_str());
         }
